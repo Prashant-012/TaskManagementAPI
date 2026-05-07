c630909db942ccdfc7dea18665b4f28c07e06a98
prashant <prashantmane7232@gmail.com>
2026-05-07 22:43:21 +0530

    Added user and taskitem model

---
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public bool IsCompleted { get; set; } = false;
+        public int Priority { get; set; } = 0; // 0: Low, 1: Medium, 2: High
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //foreign key to User
+        public int UserId { get; set; }
+
+        // Navigation property
+        public User User { get; set; } =  null;
+    }
+}
---
+++ TaskManagementAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }= String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; }  = String.Empty;
+        public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();    
+
+    }
+}
